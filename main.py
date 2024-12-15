193e2e404cf8e04103ec02c5b60ed5157f146c95
Marat <maratisimov@MacBook-Pro-Marat.local>
2024-12-15 17:51:48 +0500

    changes

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from container import get_container
 from routers import zones, places
 import uvicorn
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 container = get_container()
 app.include_router(zones.router)
